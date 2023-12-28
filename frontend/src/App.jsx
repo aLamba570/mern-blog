d0ccdbd025d71c542b303d552aab97fd6626116c
Ankush <alamba570@gmail.com>
2023-12-28 21:02:35 +0530

    minor changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import PostDetails from './pages/PostDetails';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path='/posts/post/:id' element={<PostDetails />} />
         {/* Add more routes as needed */}
       </Routes>
       
