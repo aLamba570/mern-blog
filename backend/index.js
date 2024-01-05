04030f5f2bfd6ab690d904c9e9a343c85055ac57
Ankush <alamba570@gmail.com>
2024-01-05 19:37:53 +0530

    minor changes

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const dotenv = require('dotenv')
+const path = require('path')
+const multer = require('multer')
 const cookieParser = require('cookie-parser')
 dotenv.config()
 const app = express()
@@ -15,17 +17,39 @@ app.use(
   );
 
 
+  app.use(express.json())
+  app.use(cookieParser())
+  const authRoute = require('./routes/auth')
+  const userRoute = require('./routes/User')
+  const postRoute = require('./routes/Post')
+  const commentRoute = require('./routes/Comment')
+  
+  app.use('/api/auth', authRoute)
+  app.use('/api/users', userRoute)
+  app.use('/api/posts', postRoute)
+  app.use('/api/comments', commentRoute)
+    app.use('/images', express.static(path.join(__dirname, "/images")));  
 
-const authRoute = require('./routes/auth')
-const userRoute = require('./routes/User')
-const postRoute = require('./routes/Post')
-const commentRoute = require('./routes/Comment')
-app.use(express.json())
-app.use(cookieParser())
-app.use('/api/auth', authRoute)
-app.use('/api/users', userRoute)
-app.use('/api/posts', postRoute)
-app.use('/api/comments', commentRoute)
+//image upload
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+        const originalExtension = path.extname(file.originalname);
+        cb(null, file.fieldname + "-" + uniqueSuffix + originalExtension);
+    },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+});
+
+
 
 const connectDB = async () => {
     try {
@@ -33,7 +57,7 @@ const connectDB = async () => {
         console.log("MongoDB connected");
     } catch (err) {
         console.error(err.message);
-        process.exit(1); // Exit process with failure
+        process.exit(1); // Exit process
     }
 };
 
