9565c8b096f9532a3480c890463675c4fa7ffb2a
Ankush <alamba570@gmail.com>
2024-01-17 19:51:45 +0530

    minor changes

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -1,23 +1,32 @@
-const mongoose = require('mongoose')
-const CommentSchema = new mongoose.Schema({
-    comment:{
-        type:String,
-        required:true,
-        
-    },
-    postId:{
-        type:String,
-        required:true,
-        
-    },
-    userId:{
-        type:String,
-        required:true,
-    },
-    author:{
-        type:String,
-        required:true,
-    }
-},{timestamps:true})
+// Import Mongoose module
+const mongoose = require('mongoose');
 
-module.exports = mongoose.model('Comment',CommentSchema)
+// Define CommentSchema using Mongoose Schema
+const CommentSchema = new mongoose.Schema({
+    // Comment text
+    comment: {
+        type: String,
+        required: true,
+    },
+    // Post ID to which the comment belongs
+    postId: {
+        type: String,
+        required: true,
+    },
+    // User ID who made the comment
+    userId: {
+        type: String,
+        required: true,
+    },
+    // Author of the comment
+    author: {
+        type: String,
+        required: true,
+    },
+}, {
+    // Enable timestamps to automatically add 'createdAt' and 'updatedAt' fields
+    timestamps: true
+});
+
+// Export the Comment model based on CommentSchema
+module.exports = mongoose.model('Comment', CommentSchema);
