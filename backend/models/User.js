391c4851879d77c4fa7364d7d423dd11ef8fe3ff
Ankush <alamba570@gmail.com>
2024-01-17 19:52:39 +0530

    minor changes

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,19 +1,29 @@
-const mongoose = require('mongoose')
-const UserSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    password:{
-        type:String,
-        required:true,
-    }
-},{timestamps:true})
+// Import Mongoose module
+const mongoose = require('mongoose');
 
-module.exports = mongoose.model('User',UserSchema)
+// Define UserSchema using Mongoose Schema
+const UserSchema = new mongoose.Schema({
+    // Username of the user
+    username: {
+        type: String,
+        required: true,
+        unique: true // Ensure uniqueness of usernames
+    },
+    // Email of the user
+    email: {
+        type: String,
+        required: true,
+        unique: true // Ensure uniqueness of email addresses
+    },
+    // Password of the user
+    password: {
+        type: String,
+        required: true,
+    }
+}, {
+    // Enable timestamps to automatically add 'createdAt' and 'updatedAt' fields
+    timestamps: true
+});
+
+// Export the User model based on UserSchema
+module.exports = mongoose.model('User', UserSchema);
