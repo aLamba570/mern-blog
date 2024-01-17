640232499a4a66ebb4fcdf1293407891e8bd4130
Ankush <alamba570@gmail.com>
2024-01-17 19:51:35 +0530

    minor changes

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,30 +1,42 @@
-const mongoose = require('mongoose')
+// Import Mongoose module
+const mongoose = require('mongoose');
+
+// Define PostSchema using Mongoose Schema
 const PostSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        required:true,
-        unique:true
+    // Title of the post
+    title: {
+        type: String,
+        required: true,
+        unique: true // Ensure uniqueness of post titles
     },
-    desc:{
-        type:String,
-        required:true,
+    // Description of the post
+    desc: {
+        type: String,
+        required: true,
     },
-    photo:{
-        type:String,
-        required:true,
+    // Path to the photo associated with the post
+    photo: {
+        type: String,
+        required: true,
     },
-    username:{
-        type:String,
-        required:true,
+    // Username of the author of the post
+    username: {
+        type: String,
+        required: true,
     },
-    userId:{
-        type:String,
-        required:true,
+    // User ID of the author of the post
+    userId: {
+        type: String,
+        required: true,
     },
-    categories:{
+    // Categories associated with the post (array of strings)
+    categories: {
         type: Array,
     }
-    
-},{timestamps:true})
+}, {
+    // Enable timestamps to automatically add 'createdAt' and 'updatedAt' fields
+    timestamps: true
+});
 
-module.exports = mongoose.model('Post',PostSchema)
+// Export the Post model based on PostSchema
+module.exports = mongoose.model('Post', PostSchema);
